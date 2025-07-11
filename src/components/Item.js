2ebaadf8fb2185fb52ec00ba042ec1aa918f0fa6
Denniskaranja2023! <denniswanyeki2021@gmail.com>
2025-07-11 15:43:42 +0300

    add state and events

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+   const [isAdded, setIsAdded] = useState(false);
+  function handleAddToCart() {
+    setIsAdded(!isAdded);}
+     const cartClass= isAdded ? "in-cart" : "";
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddToCart}>{isAdded?"Remove From Cart":"Add to Cart"}</button>
     </li>
   );
 }
